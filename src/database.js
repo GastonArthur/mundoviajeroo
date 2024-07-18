bbf7567d29dafe38c303c94fd028c752d16ab97f
Gaston Arthur <112443192+GastonArthur@users.noreply.github.com>
2024-07-17 22:04:12 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,10 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.host,
-    database:process.env.database,
-    user:process.env.user,
-    password:process.env.password,
+    host: process.env.mysql_host,
+    database:process.env.mysql_database,
+    user:process.env.mysql_user,
+    password:process.env.mysql_password,
     waitForConnections: true,
     connectionLimit: 5,
     queueLimit: 0
